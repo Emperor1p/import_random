c69f5d5aabdfcdc23261b69249f687e38470bedb
Peter Adewale Ayoola <122322908+Emperor1p@users.noreply.github.com>
2023-07-02 00:09:38 +0100

    Create import_random.py

---
+++ import_random.py
@@ -0,0 +1,19 @@
+import random
+for i in range(3):
+    print(random.randint(10, 20))
+
+members = ['John', 'Mary', 'Peter', 'Jack']
+leader = random.choice(members)
+print(leader)
+
+
+#dice
+class Dice:
+    def roll(self):
+        first = random.randint(1, 6)
+        second = random.randint(1, 6)
+        return first, second
+
+
+dice =  Dice()
+print(dice.roll())
